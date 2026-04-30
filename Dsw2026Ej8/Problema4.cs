7ce6bb94155a7f8885e00ce5443386eb2fd4b095
M <alumnomonzonprieto@gmail.com>
2026-04-30 20:33:34 -0300

    Problema 4

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int suma = 0;
+            int cantidad = 0;
+
+            if (nota1 == null && nota2 == null && nota3 == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int n1 = nota1 ?? -1;
+                int n2 = nota2 ?? -1;
+                int n3 = nota3 ?? -1;
+
+                int[] notas = { n1, n2, n3 };
+
+                for (int i = 0; i < 3; i++)
+                {
+                    if (notas[i]>=0 || notas[i]<=10)
+                    {
+                        suma = suma + notas[i];
+                        cantidad++;
+                    }
+                }
+                return suma / cantidad;
+            }
+
+            
+        }
+    }
+}
