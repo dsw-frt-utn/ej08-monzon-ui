a11919f916b946cb91f694b0f524d80c89aaf646
M <alumnomonzonprieto@gmail.com>
2026-04-30 20:11:18 -0300

    Problema 2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total;
+            var resumen = new {code = productCode, description = productDescription, cantidad = quantity};
+            if (resumen.cantidad <= 0)
+            {
+                total = 0;
+            }
+            else
+            {
+                total = resumen.cantidad * unitPrice;
+            }
+
+            return $"{resumen.code}-{resumen.cantidad}-{total}";
+        }
+    }
+}
