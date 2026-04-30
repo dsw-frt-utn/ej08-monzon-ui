a55b86ae5e0d180dabeb5470de64cb348fd490d9
M <alumnomonzonprieto@gmail.com>
2026-04-30 19:51:21 -0300

    Problema 1

---
+++ Dsw2026Ej8/ProductHelper.Cadena.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal partial class ProductHelper
+    {
+        public string ArmarCadena(long codigo, string descripcion, decimal precio)
+        {
+            return $"[{codigo}] {descripcion} - {precio}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal partial class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            string etiqueta = ArmarCadena(code, description, price);
+            return etiqueta;
+        }
+    }
+}
