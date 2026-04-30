b1a0aaa97cd0afc2adcf4d63df6fcfb5f2cef892
M <alumnomonzonprieto@gmail.com>
2026-04-30 20:20:40 -0300

    Problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copy = originalValue;
+            Product productref = product;
+            productref.setDescription("Descripcion modificada");
+            return $"{originalValue}-{copy}-{product.getDescription()}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Product
+    {
+        long _code;
+        string _description;
+        int _quantity;
+        decimal _price;
+
+        public Product(long code, string description, int quantity, decimal price)
+        {
+            _code = code;
+            _description = description;
+            _quantity = quantity;
+            _price = price;
+        }
+        public Product()
+        { 
+        }
+
+
+        public void setDescription(string description)
+        {
+            _description = description;
+        }
+
+        public void setCode(long code)
+        {
+            _code = code;
+        }
+        public void setQuantity(int quantity)
+        {
+            _quantity = quantity;
+        }
+
+        public void setPrice(decimal price)
+        {
+            _price = price;
+        }
+
+        public long getCode()
+        {
+            return _code;
+        }
+
+        public string getDescription()
+        {
+            return _description;
+        }
+        
+        public int getQuantity()
+        {
+            return _quantity;
+        }
+
+        public decimal getPrice()
+        {
+            return _price;
+        }
+
+
+    }
+}
