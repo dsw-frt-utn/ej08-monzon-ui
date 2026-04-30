52dd958e9e7ce5b097fa9056b28440149d9f4084
M <alumnomonzonprieto@gmail.com>
2026-04-30 20:55:13 -0300

    Problema 6

---
+++ Dsw2026Ej8/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    static class Extensions
+    {
+        static public string ToProductCode(this string code)
+        {
+            if (code == null)
+            {
+                return "SIN-CODIGO";
+            }
+            string codigoMayuscula = code.ToUpper().Trim();
+            char[] codigoCaracteres = codigoMayuscula.ToCharArray();
+
+            List<char> resultado = new List<char>();
+            bool huboEspacio = false;
+
+            foreach (char c in codigoCaracteres)
+            {
+                if (c == ' ')
+                {
+                    huboEspacio = true;
+                }
+                else
+                {
+                    if (huboEspacio)
+                    {
+                        resultado.Add('-');
+                        huboEspacio = false;
+                    }
+                    resultado.Add(c);
+                }
+            }
+
+            return new string(resultado.ToArray());
+        }
+    }
+}
